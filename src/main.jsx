900e6aaa62f68f0779cc8d4b6d9c5e5a345c02f1
José Llorens <josellorens8@gmail.com>
2023-09-22 11:10:49 -0300

    Paleta de colores

--- src/main.jsx
+++ src/main.jsx
@@ -26,13 +26,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       altImagen="Cosmetología"
     />
     <Tratamientos
-      clase="descarga"
-      nombre="Descarga"
-      descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique augue id posuere tristique. Curabitur mollis aliquam pharetra. Maecenas mollis lacinia ex ut consequat. Proin at elit fringilla, auctor libero a, aliquet dolor. Sed hendrerit lorem nunc, et tristique ex dictum id. Proin fermentum, felis non fringilla elementum, nulla sapien mattis nulla, vitae cursus orci odio nec ligula. Cras a mi ornare nibh sollicitudin mattis. Maecenas sollicitudin aliquam euismod. Nulla feugiat neque vitae odio feugiat faucibus. Aenean at felis ligula. Aliquam risus eros, efficitur a lectus vel, scelerisque pharetra lectus. Phasellus at turpis eros."
-      imagenTratamiento={imagenes.descarga}
-      altImagen="Cosmetología"
-    />
-    <Tratamientos
       clase="kiaora"
       nombre="kiaora"
       descripcion="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras tristique augue id posuere tristique. Curabitur mollis aliquam pharetra. Maecenas mollis lacinia ex ut consequat. Proin at elit fringilla, auctor libero a, aliquet dolor. Sed hendrerit lorem nunc, et tristique ex dictum id. Proin fermentum, felis non fringilla elementum, nulla sapien mattis nulla, vitae cursus orci odio nec ligula. Cras a mi ornare nibh sollicitudin mattis. Maecenas sollicitudin aliquam euismod. Nulla feugiat neque vitae odio feugiat faucibus. Aenean at felis ligula. Aliquam risus eros, efficitur a lectus vel, scelerisque pharetra lectus. Phasellus at turpis eros."
